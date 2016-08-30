ef9269f0de7f6f11a7cee2a04ec1489a244e8ec0
Codepixl <aaronsonin@yahoo.com>
2016-08-29 20:25:48 -0600

    Updated Build.Bat

--- Build.bat
+++ Build.bat
@@ -3,10 +3,10 @@ cd asm
 nasm startup.asm -f elf -o ../tmp/startup.o
 cd ../src
 i686-elf-gcc -ffreestanding -c kernel/*.c -I include/kernel -std=gnu99 -Woverflow
-move *.o ../tmp
+move *.o ../tmp 1>NUL 2>NUL
 cd ../tmp
 i686-elf-ld -o ../out/disk/codeOS2/system/krnl32.sys *.o -T ../linker.ld
 del *.o
 cd ../out
-xcopy disk D: /s /e /y
+xcopy disk D: /s /e /y 1>NUL 2>NUL
 pause
